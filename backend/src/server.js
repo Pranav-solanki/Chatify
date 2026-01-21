ee2f999e1d9814e7a258a12b7960fc5631a26516
Pranav solanki <github39645@gmail.com>
2026-01-21 23:30:13 +0530

    signup gets updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
-
+//hello its updated
 app.listen(PORT, () => {
   console.log("Server running on PORT:", PORT);
   connectDB();
